604db5893c9337c62aed317c00b661e1d3c86661
KuroNeko <kuroneko9505@gmail.com>
2019-11-23 15:49:24 +0900

    install.sh update

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 apt-get update
 apt-get upgrade
 curl -fsSL https://get.docker.com/ | sh
-apt-get install python3 python3-pip
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update
+apt-get install python3 python3-pip mongodb-org
 pip3 install -r requirements.txt
